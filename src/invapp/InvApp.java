533a491cc4f8dfdee3f7b764b29ea4c8e69de98b
Robi Irhamni <robidream20@gmail.com>
2024-03-23 13:39:11 +0700

    initial commit

---
+++ src/invapp/InvApp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package invapp;
+
+import Login.FLogin;
+
+/**
+ *
+ * @author robid
+ */
+public class InvApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FLogin frLogin = new FLogin(); 
+        frLogin.show();
+    }
+    
+}
